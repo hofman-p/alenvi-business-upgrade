fcba4f77cc5cb24e7fb853b17db0dbd31c0b25de
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-03-20 17:06:00 +0100

    Add features

---
+++ src/components/Features.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row layout-padding">
+    <div v-for="feature in featuresContent" :key="feature" class="col-sm-4 feature-padding" :style="{ backgroundColor: feature.backgroundColor }">
+      <div class="text-center">
+        <img class="main-picto" :src="feature.image" alt="">
+        <h2>{{feature.title}}</h2>
+      </div>
+      <p class="feature-text">{{feature.text}}</p>
+      <div v-if="feature.link" class="text-center">
+        <router-link class="text-primary alenvi-link" v-if="feature.link.substring(0, 4) != 'http'" :to="feature.link">En savoir plus</router-link>
+        <a v-else :href="feature.link">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    featuresContent: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.main-picto
+  width: 100px
+  height: 100px
+
+.feature-text
+  text-align: justify
+  padding-left: 10%
+  padding-right: 10%
+
+.feature-background
+  background-color: #FFFFFF
+  // background-color: #FAF9F8
+
+.feature-padding
+  padding-top: 2%
+  padding-bottom: 2%
+
+.alenvi-link
+  &:hover
+    color: #ff1693 !important
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page padding>
+    <section id="features">
+      <features :featuresContent="featuresContent"/>
+    </section>
+    <!-- <section id="auxiliaries-gallery">
+      <h1 class="text-center title-margin-bottom">Les auxiliaires d'envie</h1>
+      <auxiliaries-gallery :videoNumber="videoNumber" :videoLocation="videoLocation" :videoRoles="videoRoles"/>
+    </section> -->
+    <!-- <section id="carousel-media">
+      <h1 id="about-us" class="text-center">Ils parlent de nous</h1>
+      <carousel-media />
+    </section> -->
+    <!-- <section id="latest-articles">
+      <latest-articles />
+    </section> -->
+  </q-page>
+</template>
+
+<script>
+import Features from '../components/Features.vue'
+
+export default {
+  components: {
+    Features
+  },
+  data () {
+    return {
+      featuresContent: [
+        {
+          title: 'Aide à domicile',
+          text: 'Nous assistons votre proche pour les tâches de la vie quotidienne et passons de bons moments avec lui.',
+          link: '/aide-a-domicile',
+          image: 'https://res.cloudinary.com/alenvi/image/upload/v1507194145/images/business/pictos/aide_domicile.png',
+          backgroundColor: '#FFFFFF'
+        },
+        {
+          title: 'Compagnie & stimulation',
+          text: 'Formés en continu sur les troubles cognitifs, nous sommes là pour aider votre proche à conserver des activités : promenades, jeux...',
+          link: '/compagnie',
+          image: 'https://res.cloudinary.com/alenvi/image/upload/v1507194145/images/business/pictos/compagnie_stimulation.png',
+          backgroundColor: '#FAF9F8'
+        },
+        {
+          title: `Sortie d'hôpital`,
+          text: 'Nous aidons votre proche à retrouver son autonomie lorsqu’il regagne son domicile après un passage à l’hôpital.',
+          link: '/sortie-hopital',
+          image: 'https://res.cloudinary.com/alenvi/image/upload/v1507194145/images/business/pictos/sortie_hopital.png',
+          backgroundColor: '#FFFFFF'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
