b270ff92b34e2bd12c426a62461994f66acc73ba
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-03-21 17:01:10 +0100

    Add /contrat path and fix some paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,7 +14,7 @@ export default [
         }
       },
       {
-        path: '/compagnie',
+        path: 'compagnie',
         component: () => import('pages/Company'),
         meta: {
           breadcrumb: 'Compagnie et stimulation',
@@ -22,7 +22,7 @@ export default [
         }
       },
       {
-        path: '/sortie-hopital',
+        path: 'sortie-hopital',
         component: () => import('pages/HospitalExit'),
         meta: {
           breadcrumb: "Sortie d'hôpital",
@@ -30,7 +30,7 @@ export default [
         }
       },
       {
-        path: '/mentions',
+        path: 'mentions',
         component: () => import('pages/LegalNotices'),
         meta: {
           breadcrumb: 'Mentions Légales',
@@ -38,7 +38,7 @@ export default [
         }
       },
       {
-        path: '/recrutement',
+        path: 'recrutement',
         component: () => import('pages/Recruitment'),
         meta: {
           breadcrumb: 'Recrutement',
@@ -54,7 +54,15 @@ export default [
         }
       },
       {
-        path: '/vision',
+        path: 'contrat',
+        component: () => import('pages/Recruitment'),
+        meta: {
+          breadcrumb: 'Recrutement',
+          name: 'recrutement'
+        }
+      },
+      {
+        path: 'vision',
         component: () => import('pages/Vision'),
         meta: {
           breadcrumb: 'Vision',
@@ -62,13 +70,13 @@ export default [
         }
       },
       {
-        path: '/charte-beneficiaire',
+        path: 'charte-beneficiaire',
         beforeEnter: (to, from, next) => {
           window.location = 'https://blog.alenvi.io/charte-envie';
         }
       },
       {
-        path: '/charte-envie',
+        path: 'charte-envie',
         beforeEnter: (to, from, next) => {
           window.location = 'http://blog.alenvi.io/charte-envie';
         }
