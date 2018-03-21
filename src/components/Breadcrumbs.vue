a1b9b8703f07eb799de2daee60cae05bbae2c5e3
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-03-21 15:03:45 +0100

    Add Breadcrumbs + homecare page (+route)

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <ul class="breadcrumbs">
+      <!-- If index is last, then apply "current" css class -->
+      <li v-for="(breadcrumb, index) in $breadcrumbs" :key="index" :class="{ current: index === $breadcrumbs.length - 1 }">
+        <router-link :to="{ path: breadcrumb.meta.name }">
+          {{ breadcrumb.meta.breadcrumb }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~variables"
+
+// well it works
+  .breadcrumbs:after
+    clear: both
+    content: ''
+
+// put in list in line, add arrow after each <li> element
+  .breadcrumbs
+    list-style: none
+    li
+      float: left
+      display: inline-block
+      &:after
+        display: inline-block
+        content: '\00bb'
+        margin: 0 .6em
+        color: $tertiary
+
+// take off arrows for last <li> element
+  .breadcrumbs li:last-of-type:after
+    display: none
+
+  .breadcrumbs
+    li
+      a
+        color: $tertiary
+        &:hover
+          color: $primary
+
+  li.current > a
+    color: $primary
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,15 @@ export default [
     path: '/',
     component: () => import('layouts/Navigation'),
     children: [
-      { path: '', component: () => import('pages/Home') }
+      { path: '', component: () => import('pages/Home') },
+      {
+        path: 'aide-a-domicile',
+        component: () => import('pages/HomeCare'),
+        meta: {
+          breadcrumb: 'Aide à domicile',
+          name: 'aide-a-domicile'
+        }
+      }
     ]
   },
 
